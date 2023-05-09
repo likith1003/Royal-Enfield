56de9b4967d8c9b175dc123eaeaecae2a69ef608
likith1003 <123715227+likith1003@users.noreply.github.com>
2023-05-09 16:24:42 +0530

    Add files via upload

---
+++ RE/script.js
@@ -0,0 +1,231 @@
+function interceptor(){
+    document.getElementById('n1im').src='./images/interceptor.avif'
+    document.getElementById('rd').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function continental(){
+    document.getElementById('n1im').src='./images/continental gt.avif'
+    document.getElementById('c1').style.color='red'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function meteor650(){
+    document.getElementById('n1im').src='./images/super meteor 650.avif'
+    document.getElementById('c2').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function hunter(){
+    document.getElementById('n1im').src='./images/hunter.avif'
+    document.getElementById('c3').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function Scram(){
+    document.getElementById('n1im').src='./images/scram.avif'
+    document.getElementById('c4').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function classic(){
+    document.getElementById('n1im').src='./images/classic.avif'
+    document.getElementById('c5').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function meteor(){
+    document.getElementById('n1im').src='./images/meteor 350.avif'
+    document.getElementById('c6').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function himalayan(){
+    document.getElementById('n1im').src='./images/Himalayan.avif'
+    document.getElementById('c7').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('rd').style.color='white'
+    document.getElementById('c8').style.color='white'
+}
+function bullet(){
+    document.getElementById('n1im').src='./images/super meteor 650.avif'
+    document.getElementById('c8').style.color='red'
+    document.getElementById('c1').style.color='white'
+    document.getElementById('c2').style.color='white'
+    document.getElementById('c3').style.color='white'
+    document.getElementById('c4').style.color='white'
+    document.getElementById('c5').style.color='white'
+    document.getElementById('c6').style.color='white'
+    document.getElementById('c7').style.color='white'
+    document.getElementById('rd').style.color='white'
+    
+}
+function rets(){
+    document.getElementById('n2im').src='./images/rets.avif'
+    document.getElementById('xd').style.color='red'
+    document.getElementById('d1').style.color='white'
+    document.getElementById('d2').style.color='white'
+    document.getElementById('d3').style.color='white'
+    document.getElementById('d4').style.color='white'
+}
+function lrs(){
+    document.getElementById('n2im').src='./images/lrs.avif'
+    document.getElementById('d1').style.color='red'
+    document.getElementById('xd').style.color='white'
+    document.getElementById('d2').style.color='white'
+    document.getElementById('d3').style.color='white'
+    document.getElementById('d4').style.color='white'
+}
+function rm2(){
+    document.getElementById('n2im').src='./images/rm2.avif'
+    document.getElementById('d2').style.color='red'
+    document.getElementById('d1').style.color='white'
+    document.getElementById('xd').style.color='white'
+    document.getElementById('d3').style.color='white'
+    document.getElementById('d4').style.color='white'
+}
+function rem(){
+    document.getElementById('n2im').src='./images/rem.avif'
+    document.getElementById('d3').style.color='red'
+    document.getElementById('d1').style.color='white'
+    document.getElementById('d2').style.color='white'
+    document.getElementById('xd').style.color='white'
+    document.getElementById('d4').style.color='white'
+}
+function ss(){
+    document.getElementById('n2im').src='./images/ss.avif'
+    document.getElementById('d4').style.color='red'
+    document.getElementById('d1').style.color='white'
+    document.getElementById('d2').style.color='white'
+    document.getElementById('d3').style.color='white'
+    document.getElementById('xd').style.color='white'
+}
+function aom(){
+    document.getElementById('n3im').src='./images/aom.avif'
+    document.getElementById('yd').style.color='red'
+    document.getElementById('e1').style.color='white'
+    document.getElementById('e2').style.color='white'
+    document.getElementById('e3').style.color='white'
+    document.getElementById('e4').style.color='white'
+    document.getElementById('e5').style.color='white'
+}
+function sm6(){
+    document.getElementById('n3im').src='./images/sm6.avif'
+    document.getElementById('e1').style.color='red'
+    document.getElementById('yd').style.color='white'
+    document.getElementById('e2').style.color='white'
+    document.getElementById('e3').style.color='white'
+    document.getElementById('e4').style.color='white'
+    document.getElementById('e5').style.color='white'
+}
+function tcc(){
+    document.getElementById('n3im').src='./images/tcc.avif'
+    document.getElementById('e1').style.color='red'
+    document.getElementById('e1').style.color='white'
+    document.getElementById('yd').style.color='white'
+    document.getElementById('e3').style.color='white'
+    document.getElementById('e4').style.color='white'
+    document.getElementById('e5').style.color='white'
+}
+function rea(){
+    document.getElementById('n3im').src='./images/rea.avif'
+    document.getElementById('e3').style.color='red'
+    document.getElementById('e1').style.color='white'
+    document.getElementById('e2').style.color='white'
+    document.getElementById('yd').style.color='white'
+    document.getElementById('e4').style.color='white'
+    document.getElementById('e5').style.color='white'
+}
+function rex(){
+    document.getElementById('n3im').src='./images/rex.avif'
+    document.getElementById('e4').style.color='red'
+    document.getElementById('e1').style.color='white'
+    document.getElementById('e2').style.color='white'
+    document.getElementById('e3').style.color='white'
+    document.getElementById('yd').style.color='white'
+    document.getElementById('e5').style.color='white'
+}
+function mrj(){
+    document.getElementById('n3im').src='./images/mrj.avif'
+    document.getElementById('e5').style.color='red'
+    document.getElementById('e1').style.color='white'
+    document.getElementById('e2').style.color='white'
+    document.getElementById('e3').style.color='white'
+    document.getElementById('e4').style.color='white'
+    document.getElementById('yd').style.color='white'
+}
+function miy(){
+    document.getElementById('n4im').src='./images/miy.avif'
+    document.getElementById('f1').style.color='red'
+    document.getElementById('zd').style.color='white'
+}
+function gma(){
+    document.getElementById('n4im').src='./images/gma.avif'
+    document.getElementById('zd').style.color='red'
+    document.getElementById('f1').style.color='white'
+}
+function s(){
+    document.getElementById('n5im').src='./images/s.avif'
+    document.getElementById('ad').style.color='red'
+    document.getElementById('z1').style.color='white'
+    document.getElementById('z2').style.color='white'
+}
+function sc(){
+    document.getElementById('n5im').src='./images/sc.avif'
+    document.getElementById('z1').style.color='red'
+    document.getElementById('ad').style.color='white'
+    document.getElementById('z2').style.color='white'
+}
+function gpd(){
+    document.getElementById('n5im').src='./images/gpd.avif'
+    document.getElementById('z2').style.color='red'
+    document.getElementById('ad').style.color='white'
+    document.getElementById('z1').style.color='white'
+}
